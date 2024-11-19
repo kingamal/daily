2ef9001d92901cfb02384904cdb37a9efee8803a
Kinga <kinga.maleszewska@o2.pl>
2024-11-19 14:28:26 +0100

    Simplify the code

--- drawing_app.py
+++ drawing_app.py
@@ -5,19 +5,14 @@ window=Tk()
 def pick_color():
     print('color')
 
-b1=Button(window, text="Pick Color", command=pick_color)
-b1.grid(row=1, column=0)
+b1=Button(window, text="Pick Color", command=pick_color).grid(row=1, column=0)
 
-l1=Label(window, text="Brush Size: ")
-l1.grid(row=1, column=1)
+l1=Label(window, text="Brush Size: ").grid(row=1, column=1)
 
-b2=Button(window, text="Clear", command=pick_color)
-b2.grid(row=1, column=2)
+b2=Button(window, text="Clear", command=pick_color).grid(row=1, column=2)
 
-b3=Button(window, text="Save Drawing", command=pick_color)
-b3.grid(row=1, column=3)
+b3=Button(window, text="Save Drawing", command=pick_color).grid(row=1, column=3)
 
-b4=Button(window, text="Exit", command=window.destroy)
-b4.grid(row=1, column=4)
+b4=Button(window, text="Exit", command=window.destroy).grid(row=1, column=4)
 
 window.mainloop()
