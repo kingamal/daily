7a38e7d2ddb913d58e72eda76cb08dd7b9320ac6
Kinga <kinga.maleszewska@o2.pl>
2024-12-27 14:38:38 +0100

    Improve the organizer witch choosing the options

--- movies.py
+++ movies.py
@@ -1,13 +1,51 @@
-FAVORITE_MOVIES = ['Inception', 'The Matrix', 'Interstellar', 'The Dark Knight']
+def display_menu():
+    print("\nPlease select an option:")
+    print("1. Add a new movie")
+    print("2. Remove a movie")
+    print("3. Total number of favorite movies")
+    print("4. The movies in alfabethical order")
+    print("5. Exit")
 
-def movies(favorite_movies):
-    favorite_movies.append('The Godfather')
-    favorite_movies.remove('The Matrix')
-    print(f'Total number of movies: {len(favorite_movies)}')
-    favorite_movies.sort()
+def add_movie(movies):
+    movie = input("Enter the new movie to add: ")
+    movies.append(movie)
+
+def remove_movie(movies):
+    movie = input("Enter the movie to remove: ")
+    movies.remove(movie)
+
+def total_number(movies):
+    print(f'Total number of movies: {len(movies)}')
+
+def sorting_movies(movies):
+    movies.sort()
     print("Your favorite movies in alphabetical order:")
-    for movie in favorite_movies:
+    for movie in movies:
         print(f"- {movie}")
 
+def main():
+    movies = []
+    while True:
+        display_menu()
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 5.")
+            continue
+
+        if choice == 1:
+            add_movie(movies)
+        elif choice == 2:
+            remove_movie(movies)
+        elif choice == 3:
+            total_number(movies)
+        elif choice == 4:
+            sorting_movies(movies)
+        elif choice == 5:
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please choose a number between 1 and 5.")
+
 if __name__ == '__main__':
-    movies(FAVORITE_MOVIES)
+    main()
