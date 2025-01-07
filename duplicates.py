a212fdbab4e25514c8643a398bce92ec7430cef6
Kinga <kinga.maleszewska@o2.pl>
2025-01-07 21:44:32 +0100

    Adding second solution for removing duplicates in lists

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,6 @@
 numbers = [3, 1, 2, 3, 4, 1, 5, 2]
 
+# First solution
 def remove_duplicates(input_list):
     unique_list = []
     for item in input_list:
@@ -8,4 +9,8 @@ def remove_duplicates(input_list):
     return unique_list
 
 cleaned_list = remove_duplicates(numbers)
+print("List without duplicates:", cleaned_list)
+
+# Second solution - cleaner and more efficient
+cleaned_list = list(dict.fromkeys(numbers))
 print("List without duplicates:", cleaned_list)
