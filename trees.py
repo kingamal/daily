7de6e7e8c964f63606f6586ea001253c3fb1878d
Kinga <kinga.maleszewska@o2.pl>
2025-01-16 19:28:20 +0100

    Simplify the task by using python library collections

--- trees.py
+++ trees.py
@@ -1,12 +1,16 @@
+from collections import Counter
+
 trees = ["oak", "pine", "maple", "oak", "birch", "pine", "oak"]
 
 tree_counts = {}
 
-for tree in trees:
-    if tree in tree_counts:
-        tree_counts[tree] += 1
-    else:
-        tree_counts[tree] = 1
+# for tree in trees:
+#     if tree in tree_counts:
+#         tree_counts[tree] += 1
+#     else:
+#         tree_counts[tree] = 1
+
+tree_counts = Counter(trees)
 
 for species, count in tree_counts.items():
     print(f"{species}: {count}")
